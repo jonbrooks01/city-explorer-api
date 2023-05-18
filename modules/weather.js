8f19e9b23dc22a55a10b22ea36b23fc6fd00e36f
Jonathan Brooks <jonbrooks0122@gmail.com>
2023-05-17 20:27:26 -0700

    finsihed lab 10

--- modules/weather.js
+++ modules/weather.js
@@ -1,14 +1,34 @@
 'use strict'
 
 const axios = require('axios');
+const cache = require('./cache')
+
+
 
 function getWeather (request, response, next) {
   const {lat,lon} = request.query
     const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`
-    axios.get(url)
-    .then(res => res.data.data.map(weather => new Forecast(weather)))
-    .then(formattedData => response.status(200).send(formattedData))
-    .catch(err => next(err))
+    const key = 'obj' + lat + lon
+
+    console.log(cache)
+
+    if(cache[key] && (Date.now() - cache[key].timestamp < 43200000)) {
+      console.log('cache hit - sending date from cache');
+      response.status(200).send(cache[key].data)
+    }
+    else{
+      console.log('cache miss - making a new request to the API')
+      axios.get(url)
+      .then(res => res.data.data.map(weather => new Forecast(weather)))
+      .then(formattedData => {
+        cache[key] = {};
+        cache[key].data = formattedData
+        cache[key].timestamp = Date.now();
+
+        response.status(200).send(formattedData)
+      })
+      .catch(err => next(err))
+    }
   }
 
 class Forecast {
